9160a9376e01e11a75e2e76899ec8d9ef7d984be
KhaledBadrash <168454366+KhaledBadrash@users.noreply.github.com>
2024-05-29 14:24:28 +0200

    d

--- Khaled.py
+++ Khaled.py
@@ -1,1 +1,16 @@
-print("salam")
+from textual.app import App, ComposeResult
+from textual.widgets import Static
+
+
+class WidgetApp(App):
+    def compose(self) -> ComposeResult:
+        self.widget = Static("Textual")
+        yield self.widget
+
+    def on_mount(self) -> None:
+        self.widget.styles.background = "darkblue"
+        self.widget.styles.border = ("heavy", "white")
+
+if __name__ == "__main__":
+    app = WidgetApp()
+    app.run()
