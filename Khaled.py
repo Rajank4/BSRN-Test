1dd5fbfeec185d88a90929c14c3faf487c4a8eb5
KhaledBadrash <168454366+KhaledBadrash@users.noreply.github.com>
2024-06-03 17:05:52 +0200

    Khaled/Mohamed

--- Khaled.py
+++ Khaled.py
@@ -1,40 +1,5 @@
-from textual.app import App, ComposeResult
-from textual.widgets import Static, Button
-from textual.widgets import Welcome
+#Anz Spieler, größe des Feldes,
+print("Wilkommen zum Buzzword Bingospiel")
+anz_Spieler = int(input("Geben sie die Anzahl der Spieler an:"))  #fehler abfangen
 
-
-class TestButton(Static):
-
-    def compose(self):
-        for i in range(0,5):
-            yield Button("Button 1")
-
-
-class WidgetApp(App):
-    def compose(self) -> ComposeResult:
-        self.widget = Static("Buzzword Bingo")
-        yield self.widget
-
-    # def for schleife für bestimmung der laenge
-
-    def Zeilen(self): #addet Zeilen
-        for i in range(0,5):
-            yield TestButton()
-
-
-    CSS = """
-    TestButton {
-        layout: horizontal;
-    }
-    """
-
-    def on_mount(self) -> None:
-        self.widget.styles.background = "aquamarine"
-        self.widget.styles.color = "black"
-        self.widget.styles.border = ("heavy", "black")
-        self.widget.styles.width = 17
-
-
-if __name__ == "__main__":
-    app = WidgetApp()
-    app.run()
+feld_gr = int(input("Geben sie die größe ihres Spielfeldes: \n 1 = 3X3 \n 2 = 5X5 \n 3 = 7X7"))
